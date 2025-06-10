c952b141fd416fdc47af7f31acd7a667e364e129
DamiannDiYorio <damiangongoradiyorio51@gmail.com>
2025-06-10 10:32:08 -0300

    First commit

---
+++ ArbolBinario.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace tp2
+{
+	public class ArbolBinario<T>
+	{
+		
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+
+		private ArbolBinario<T> padre = null;
+
+		private int altura = 0;
+		
+		public ArbolBinario(T dato)
+		{
+			this.dato = dato;
+		}
+		public ArbolBinario<T> getPadre()
+		{
+			return this.padre;
+		}
+
+		public void setPadre(ArbolBinario<T> nuevoPadre)
+		{
+			this.padre = nuevoPadre;
+		}
+
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public ArbolBinario<T> getHijoIzquierdo() {
+			return this.hijoIzquierdo;
+		}
+	
+		public ArbolBinario<T> getHijoDerecho() {
+			return this.hijoDerecho;
+		}
+
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo)
+		{
+			this.hijoIzquierdo = hijo;
+			if (hijo != null) 
+			{
+				hijo.padre = this;
+				this.PropagarAltura();
+			}
+		}
+
+		public void agregarHijoDerecho(ArbolBinario<T> hijo)
+		{
+			this.hijoDerecho = hijo;
+			if (hijo != null) 
+			{
+				hijo.padre = this;
+				this.PropagarAltura();
+			}
+		}
+	
+		public void eliminarHijoIzquierdo() {
+			this.hijoIzquierdo=null;
+		}
+	
+		public void eliminarHijoDerecho() {
+			this.hijoDerecho=null;
+		}
+	
+		public bool esHoja() {
+			return this.hijoIzquierdo==null && this.hijoDerecho==null;
+		}
+		
+		public void inorden() {
+		}
+		
+		public void preorden() {
+		}
+		
+		public void postorden() {
+		}
+		
+		public void recorridoPorNiveles() {
+		}
+	
+		public int contarHojas() {
+			return 0;
+		}
+		
+        public int Altura
+        {
+            get { return altura; }
+        }
+		private int getAltura(ArbolBinario<T> nodo)
+		{
+			return nodo == null ? -1 : nodo.altura;
+		}
+
+		private void setAltura()
+		{
+			this.altura = Math.Max(getAltura(this.hijoIzquierdo), getAltura(this.hijoDerecho)) + 1;
+		}
+
+		
+		private void PropagarAltura()
+		{
+			ArbolBinario<T> nodo = this;
+			while (nodo != null)
+			{
+				nodo.setAltura();
+				nodo = nodo.padre;
+			}
+		}
+
+		public void recorridoEntreNiveles(int n, int m)
+		{
+		}
+	}
+}
---
+++ Estrategia.cs
@@ -0,0 +1,116 @@
+﻿
+using System;
+using System.Collections.Generic;
+using tp1;
+using tp2;
+
+namespace tpfinal
+{
+
+	class Estrategia
+	{
+
+
+		public string Consulta1(ArbolBinario<DecisionData> arbol)
+		{
+			string text = _Consulta1(arbol);
+
+			int count = 0;
+			foreach (char character in text)
+			{
+				if (character == '%')
+				{
+					count++;
+				}
+			}
+			if (count == 0)
+			{
+				count = 1;
+			}
+
+			return text.Replace("100%", (100 / count) + "%");
+		}
+		private String _Consulta1(ArbolBinario<DecisionData> arbol)
+		{
+			if (arbol.esHoja())
+			{
+				return arbol.getDatoRaiz().ToString() + "\n";
+			}
+
+			string izquierdo = _Consulta1(arbol.getHijoIzquierdo());
+			string derecho = _Consulta1(arbol.getHijoDerecho());
+
+
+			return izquierdo + derecho;
+		}
+
+		public String Consulta2(ArbolBinario<DecisionData> arbol)
+		{
+			return _Consulta2(arbol, "", "").Replace("== no?", "").Replace("== si?", "").Replace("{0}? == ", "").Replace("{0} ", "").Replace(":100% ", "");
+		}
+		public string _Consulta2(ArbolBinario<DecisionData> _arbol, string _camino, string _lista_camino)
+		{
+			_camino += _arbol.getDatoRaiz().ToString();
+
+			if (_arbol.esHoja())
+			{
+				_lista_camino += _camino + "\n";
+			}
+			else
+			{
+				_lista_camino = _Consulta2(_arbol.getHijoIzquierdo(), _camino + "→ si →", _lista_camino);
+				_lista_camino = _Consulta2(_arbol.getHijoDerecho(), _camino + "→ no →", _lista_camino);
+			}
+			return _lista_camino;
+		}
+
+		public String Consulta3(ArbolBinario<DecisionData> arbol)
+		{
+			Cola<ArbolBinario<DecisionData>> cola = new Cola<ArbolBinario<DecisionData>>();
+			string resultado = "";
+
+			List<ArbolBinario<DecisionData>> list = new List<ArbolBinario<DecisionData>>();
+			int cont = 1;
+			cola.encolar(arbol);
+
+			while (!cola.esVacia())
+			{
+				resultado += $"\n nivel {cont}: \n";
+				cont++;
+
+				while (!cola.esVacia())
+				{
+					list.Add(cola.desencolar());
+				}
+
+				foreach (ArbolBinario<DecisionData> a in list)
+				{
+					resultado += a.getDatoRaiz() + " | ";
+					if (!a.esHoja())
+					{
+						cola.encolar(a.getHijoIzquierdo());
+						cola.encolar(a.getHijoDerecho());
+					}
+				}
+				list = new List<ArbolBinario<DecisionData>>();
+			}
+			return resultado.Replace("== no?", "").Replace("== si?", "").Replace("{0}? == ", "").Replace("{0} ", "").Replace(":100% ", "");
+		}
+
+		public ArbolBinario<DecisionData> CrearArbol(Clasificador clasificador)
+		{
+			if (clasificador.crearHoja())
+			{
+				return new ArbolBinario<DecisionData>(new DecisionData(clasificador.obtenerDatoHoja()));
+			}
+
+			ArbolBinario<DecisionData> nuevoArbol = new ArbolBinario<DecisionData>(new DecisionData(clasificador.obtenerPregunta()));
+			nuevoArbol.agregarHijoIzquierdo(CrearArbol(clasificador.obtenerClasificadorIzquierdo()));
+			nuevoArbol.agregarHijoDerecho(CrearArbol(clasificador.obtenerClasificadorDerecho()));
+
+			return nuevoArbol;
+		}
+		
+
+	}
+}
